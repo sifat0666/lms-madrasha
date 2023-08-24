326ea129c3c8bcb8bc1d607995139b8c40e1ede0
sifat <sifat0666@gmail.com>
2023-08-24 23:42:57 +0600

    contact changed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,7 +5,9 @@ const ContactUsPage = () => {
     <div className="container py-5 bg-light vh-100 vw-100">
       <div className="row">
         <div className="col-md-12">
-          <h1 className="waterfall-header text-center mb-4">Water Drop IT</h1>
+          <h1 className="waterfall-header text-center mb-4">
+            Milky Way It Solution
+          </h1>
         </div>
         <div className="col-md-12">
           {/* <h2 className="text-center mb-3">Contact Us</h2>
